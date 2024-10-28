c5477df8c4b8286ad80fe24288aaa5c61929cf22
khushal vadher aubie <khushal@auberginesolutions.com>
2024-10-28 18:32:42 +0530

    code

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import "module-alias/register";
+import express, { Request, Response } from "express";
+import type { Express } from "express";
+import { Router } from "express";
+import { successResponse } from "./resGen";
+
+class Server {
+  public app: Express;
+  public router = express.Router();
+  public allRoutes: Router;
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  public config() {
+    this.app.use(express.json());
+  }
+
+  public routes() {
+    this.app.get("/", (req: Request, res: Response) => {
+      return successResponse(res, 200, "Hello World");
+
+      console.log("first");
+    });
+  }
+
+  public start() {
+    this.app.listen(5000, () => {
+      console.log("running");
+    });
+  }
+}
+
+const server = new Server();
+server.start();
